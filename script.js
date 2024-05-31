d8177e7ca944fc8a580f92f8c4b823e24cac3566
KienHongNguyen <hongkien66@gmail.com>
2024-05-31 16:08:33 +0700

    finished page

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-console.log ("hi");
 const expandIcon = document.querySelector("#expand-icon");
-console.log (expandIcon);
 expandIcon.addEventListener("click", () => {
     console.log();
 })
 
+const menu = document.querySelector("#expand-icon")
+
+menu.addEventListener("click", function() {
+    expandSideBar()
+})
+
+function expandSideBar() {
+    document.querySelector("body").classList.toggle("short")
+
+}
